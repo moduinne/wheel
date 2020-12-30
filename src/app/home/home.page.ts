07a68198fb826dc200ca5babcd441b377a2af2a6
ethanol190 <cloudygumption@gmail.com>
2020-12-30 22:00:15 +1000

    added ability to change residues, needs help with remembering the indexChosen for pgs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'; 
 import { WheelSelector } from '@ionic-native/wheel-selector/ngx';
-import { NavController, ToastController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 import { AminoAcid } from '../amino-acid';
 import { AminoAcidService } from '../amino-acid.service';
 import { ProtectionGroup } from '../protection-group';
@@ -12,51 +12,37 @@ import { ProtectionService } from '../protection.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit{
-
-  //  public testOut:string = "placeholder";
-  //  public aminoOne = new AminoAcid("test","Tes","Z",3,"Tes");
-  //  public testProtOne = new ProtectionGroup("tpgone",1,"tpgone");
-  //  public testProtTwo = new ProtectionGroup("tpgtwo",2,"tpgtwo");
-
-  //  public aminoTwo = new AminoAcid("AAA","AAA","A",3434,"AAA");
-  //  public testProtThree = new ProtectionGroup("tpgthrees",13434,"tpgthrees");
-  //  public testProtFour = new ProtectionGroup("tpgFours",3434,"tpgFours");
-
-  //  public protOne = [this.testProtOne,this.testProtThree];
-  //  public peptide = [this.aminoOne,this.aminoTwo];
-  //  public protTwo = [this.testProtTwo,this.testProtFour];
     
   public protGrps = [];
   public aminoAcids = []
-
   public protOne = [];
   public peptide = [];
   public protTwo = [];
-
-  public massCalc:number = 0;
+  public massCalc = 0;
  
-
   constructor(public navCtrl:NavController,
-    private svc:AminoAcidService,
+    private svcAA:AminoAcidService,
     private svcPG:ProtectionService,
-    private selector:WheelSelector,
-    public toastCtrl:ToastController
+    private selector:WheelSelector
     ) {}
 
+  /**The first code to be run on initialising */
   ngOnInit(): void {
     this.populateAminoAcids();
     this.populateProtectionGroups();
-    this.calculateMass();
   }
 
+  /**populates the available amino-acids from assets folder json via service */
   public populateAminoAcids() {
-    this.svc.getAllAminoAcids().subscribe((res) => this.aminoAcids = res);
+    this.svcAA.getAllAminoAcids().subscribe((res) => this.aminoAcids = res);
   }
 
+  /**populates the available protective-groups from assets folder json via service */
   public populateProtectionGroups() {
     this.svcPG.getAllProtectionGroups().subscribe((res) => this.protGrps = res);
   }
 
+  /**Removes the specified residue and updates the calculated mass */
   public removeResidue(i) {
     this.protOne.splice(i,1);
     this.peptide.splice(i,1);
@@ -64,6 +50,47 @@ export class HomePage implements OnInit{
     this.calculateMass();
   }
 
+  /**Opens the peptide builder at the position chosen in app for editing */
+  public openPickerAtIndex(i){
+    let indexChosen:number = i;
+    this.selector.show({
+      title:'Peptide-Builder',
+      positiveButtonText:'Select',
+      negativeButtonText:'Cancel',
+      items:[
+        this.protGrps,
+        this.aminoAcids,
+        this.protGrps
+      ],
+      defaultItems: [
+        {index:0, value:this.protGrps[indexChosen].description},
+        {index:1, value:this.aminoAcids[indexChosen].description},
+        {index:2, value:this.protGrps[indexChosen].description}
+      ]
+    }).then((result) => {
+      for(let a of this.aminoAcids){
+        if(a.description === result[1].description) {
+          let residue = new AminoAcid(a.name,a.triple,a.single,a.mass,a.description);
+          this.peptide[indexChosen] = residue;
+        }
+      }
+      for (let p of this.protGrps){
+        if(p.description === result[0].description) {
+          let protective = new ProtectionGroup(p.name,p.mass,p.description);
+          this.protOne[indexChosen] = protective;
+        }
+      }
+      for (let p of this.protGrps){
+        if(p.description === result[2].description) {
+          let protective = new ProtectionGroup(p.name,p.mass,p.description);
+          this.protTwo[indexChosen] = protective;
+        }
+      }
+      this.calculateMass();
+    });
+  }
+
+  /**opens the picker when adding a new residue to the growing peptide */
   public openPicker(){
     this.selector.show({
       title:'Peptide-Builder',
@@ -102,17 +129,18 @@ export class HomePage implements OnInit{
     });
   }
 
-  public calculateMass() {
+  /**Calculates the mass based resdiues and protective groups present in the peptide */
+  private calculateMass() {
     let localMass:number = 0;
     let waterWeight = (this.peptide.length - 1) * 18;
     for(let i = 0 ; i < this.peptide.length ; i++) {
       localMass += parseInt(this.peptide[i].mass);
       localMass += parseInt(this.protOne[i].mass);
       localMass += parseInt(this.protTwo[i].mass);
-      // localMass += this.peptide[i].mass;
-      // localMass +=this.protOne[i].mass;
-      // localMass += this.protTwo[i].mass;
     }
     this.massCalc = localMass - waterWeight;
+    if(this.peptide.length === 0) {
+      this.massCalc = 0;
+    }
   }
 }
